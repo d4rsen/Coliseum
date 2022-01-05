f4a2adbece8ec1ccf841bb0611017d224c96471d
darsen <dlya_web_konkursa@mail.ru>
2022-01-05 13:09:01 +0300

    wejfhweuhrwru3w

--- src/components/common/PlayerProgressBarHpApMp/PlayerProgressBarHpApMp.jsx
+++ src/components/common/PlayerProgressBarHpApMp/PlayerProgressBarHpApMp.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 
 const PlayerProgressBarHpApMp = ({bgColor, stat}) => {
-  return (
-    <div>
-      <div className="progress">
-        <div
-          className={`progress-bar progress-bar-striped progress-bar-animated ${bgColor}`}
-          role="progressbar"
-          style={{width: `${stat}%`}}
-          aria-valuenow="100"
-          aria-valuemin="0"
-          aria-valuemax="100"
-        >
-          {stat}
+    return (
+        <div>
+            <div className="progress">
+                <div
+                    className={`progress-bar progress-bar-striped progress-bar-animated ${bgColor}`}
+                    role="progressbar"
+                    style={{width: `${stat}%`}}
+                    aria-valuenow="100"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                >
+                    {stat}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default PlayerProgressBarHpApMp
