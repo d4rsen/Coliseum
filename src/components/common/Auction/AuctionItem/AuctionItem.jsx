21b71663fad74afce0596f3dcaecbbdd6097ebbe
Aleksey Mityukov <imondoro@gmail.com>
2022-01-13 14:15:29 +0300

    auction init

---
+++ src/components/common/Auction/AuctionItem/AuctionItem.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const AuctionItem = () => {
+    const itemImg = require()
+    const itemName = 'Great Axe'
+    const itemStats = {
+        dmg: 19,
+        str: 7,
+        agl: 5,
+        int: 3
+    }
+    const itemInfo = 'Lorem ipsum dolor sit amet, consectetur ' +
+        'adipisicing elit. Explicabo facilis laborum nihil nostrum' +
+        ' repudiandae. Aut cumque ex, facilis harum iste, iusto' +
+        ' minus mollitia nihil pariatur placeat, recusandae rerum' +
+        ' vero voluptatibus?'
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default AuctionItem;
---
+++ src/components/routing/private/Auction/Auction.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Auction = () => {
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Auction;
