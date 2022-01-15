56be0946e59ac14bd3ad2a5c3b8a55e94fccdc2b
Aleksey Mityukov <imondoro@gmail.com>
2022-01-15 15:06:57 +0300

    auction fix

--- src/redux/reducers/auctionReducer.js
+++ src/redux/reducers/auctionReducer.js
@@ -1,7 +1,12 @@
-import initialState from '../init/initialState'
-import { GET_ALL_AUC_ITEMS, POST_AUCTION_ITEM } from '../types/auctionTypes'
+import InitialState from '../init/initialState'
+import {GET_ALL_AUC_ITEMS, POST_AUCTION_ITEM} from '../types/auctionTypes'
 
+<<<<<<< Updated upstream
 export const auctionReducer = (state = initialState, action) => {
+=======
+export const auctionReducer = (state = InitialState, action) => {
+    console.log(action.payload)
+>>>>>>> Stashed changes
     switch (action.type) {
         case POST_AUCTION_ITEM:
             console.log(action.payload)
