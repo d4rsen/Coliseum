894ca1544ac98aefd160e8b71d1aad49dd37573d
darsen <dlya_web_konkursa@mail.ru>
2022-01-16 16:58:55 +0300

    andr MERGED

--- src/components/common/PlayerStats/PlayerStats.jsx
+++ src/components/common/PlayerStats/PlayerStats.jsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import styles from './PlayerStats.module.css';
+import React from 'react'
+import { useSelector } from 'react-redux'
+import styles from './PlayerStats.module.css'
 
 const PlayerStats = () => {
-  const player = useSelector((state) => state.player);
+    const player = useSelector((state) => state.player)
+    console.log(player.total_stats)
+    const stats = Object.entries(player.total_stats).slice(6, 10)
+    console.log(stats)
+    return (
+        <ul className={styles.player__stats}>
+            {stats.map((item, i) => {
+                return (<li>{item[0]},{item[1]}</li>)
+            })}
 
-  console.log(player.total_stats);
-  const stats = Object.entries(player.total_stats);
-  console.log(stats);
-  return (
-    <ul className={styles.player__stats}>
-      {stats.map((item, i) => {
-        return (<li>{item[i]}</li>)
-      })}
-
-      {/* <li>agl: {player.total_stats.agl}</li>
+            {/* <li>agl: {player.total_stats.agl}</li>
       <li>int: {player.total_stats.int}</li>
       <li>def: {player.total_stats.def}</li>
       <li>evs: {player.total_stats.evs}</li> */}
-    </ul>
-  );
-};
+        </ul>
+    )
+}
 
-export default PlayerStats;
+export default PlayerStats
