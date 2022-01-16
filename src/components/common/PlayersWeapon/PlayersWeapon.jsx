fd434e067b9877c4f618742d112262bd236ce45e
darsen <dlya_web_konkursa@mail.ru>
2022-01-17 00:08:08 +0300

    battle 05

--- src/components/common/PlayersWeapon/PlayersWeapon.jsx
+++ src/components/common/PlayersWeapon/PlayersWeapon.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import styles from './PlayersWeapon.module.css';
+import React from 'react'
+import styles from './PlayersWeapon.module.css'
 
 const PlayerWeapon = ({url, width, height}) => {
     // const player = useSelector((state) => state.player);
-    console.log(width)
     const sword =
-        'https://freepngimg.com/thumb/sword/31768-7-black-sword-thumb.png';
+        'https://freepngimg.com/thumb/sword/31768-7-black-sword-thumb.png'
 
     return (
         <div>
             <img width={`${width}%`} height={`${height}px`}
                  className={styles.player__weapon}
-                 src={sword} alt='sdfsdf'></img>
+                 src={sword} alt="sdfsdf"></img>
             <p>str 500</p>
         </div>
     )
-};
+}
 
-export default PlayerWeapon;
+export default PlayerWeapon
