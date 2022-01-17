51fc589fc264eff2026d802027f9a8814adf3b11
Andrew <isofthetruth@gmail.com>
2022-01-17 11:16:22 +0300

    person релиз-2

--- src/components/common/PlayerProgressBarHpApMp/PlayerProgressBarHpApMp.jsx
+++ src/components/common/PlayerProgressBarHpApMp/PlayerProgressBarHpApMp.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
-import './PlayerProgressBarHpApMp.module.css'
+import React from 'react';
+import './PlayerProgressBarHpApMp.module.css';
 
-const PlayerProgressBarHpApMp = ({bgColor, stat, height, fontSize}) => {
-    return (
+const PlayerProgressBarHpApMp = ({ bgColor, stat, height, fontSize, type, rotate }) => {
+ console.log(rotate)
+  return (
+    <>
+      {type ===
+        'header' &&(
+          <div
+            style={{
+              width: `${stat}%`,
+              height: `${height}px`,
+              backgroundColor: bgColor,
+            }}
+          >
+            <span style={{ fontSize: `${fontSize}px` }}>{stat}</span>
+          </div>
+        )}
+         {type ===
+        'player' &&(
+          <div
+            style={{
+              width: `${20}%`,
+              height: `${stat}%`,
+              backgroundColor: bgColor,
+            }}
+          >
+            <p  style={{ fontSize: `${fontSize}px` ,transform:`rotateX(${rotate}deg)` }}>{stat}</p>
+          </div>
+        )}
+    </>
+  );
+};
 
-        <div style={{
-            width: `${stat}%`, height: `${height}px`,
-            backgroundColor: bgColor
-        }}>
-            <span style={{fontSize: `${fontSize}px`}}>{stat}</span>
-        </div>
-    )
-}
-
-export default PlayerProgressBarHpApMp
+export default PlayerProgressBarHpApMp;
