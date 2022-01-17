163d19a107b9cb027873cb8c284e1d8ad4f70458
Aleksey Mityukov <imondoro@gmail.com>
2022-01-17 11:46:04 +0300

    player ** dev ** battle ** backpack

--- src/components/common/Player/Player.jsx
+++ src/components/common/Player/Player.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import {useSelector} from 'react-redux';
 import EmptyItem from '../EmptyItem/EmptyItem';
 import style from './Player.module.css';
 import PlayerProgressBarHpApMp from '../PlayerProgressBarHpApMp/PlayerProgressBarHpApMp';
@@ -7,117 +7,118 @@ import PlayerStats from '../PlayerStats/PlayerStats';
 import PlayerWeapon from '../PlayersWeapon/PlayersWeapon';
 
 const Player = () => {
-  const player = useSelector((state) => state.player);
+    const player = useSelector((state) => state.player);
 
-  const playerClass = player.playerClass;
-  console.log(playerClass);
-  const link =
-    'https://is3-ssl.mzstatic.com/image/thumb/Purple118/v4/6b/f3/a6/6bf3a6b2-85de-5e23-cb92-0d739a178c53/source/512x512bb.jpg';
-  const playerAvatar =
-    'https://www.pngall.com/wp-content/uploads/4/Samurai-PNG-Image.png';
-  const sword =
-    'https://freepngimg.com/thumb/sword/31768-7-black-sword-thumb.png';
-  // const xz = 'https://pic.xenomorph.ru/2019-03/1553747403_necropolis5.jpg';
-  // const test =
-  //   'https://dbforgame.herokuapp.com/static/img/classes/playerClasses/assasin-wonam.jpg';
+    const playerClass = player.playerClass;
+    console.log(playerClass);
+    const link =
+        'https://is3-ssl.mzstatic.com/image/thumb/Purple118/v4/6b/f3/a6/6bf3a6b2-85de-5e23-cb92-0d739a178c53/source/512x512bb.jpg';
+    const playerAvatar =
+        'https://www.pngall.com/wp-content/uploads/4/Samurai-PNG-Image.png';
+    const sword =
+        'https://freepngimg.com/thumb/sword/31768-7-black-sword-thumb.png';
+    // const xz = 'https://pic.xenomorph.ru/2019-03/1553747403_necropolis5.jpg';
+    // const test =
+    //   'https://dbforgame.herokuapp.com/static/img/classes/playerClasses/assasin-wonam.jpg';
 
-  const classAssasin =
-    'https://dbforgame.herokuapp.com/static/img/classes/playerClasses/assasin-wonam.jpg';
+    const classAssasin =
+        'https://dbforgame.herokuapp.com/static/img/classes/playerClasses/assasin-wonam.jpg';
 
-  const classWarrior =
-    'https://dbforgame.herokuapp.com/static/img/classes/playerClasses/warrior_man.jpg';
+    const classWarrior =
+        'https://dbforgame.herokuapp.com/static/img/classes/playerClasses/warrior_man.jpg';
 
-  const classMonk =
-    'https://dbforgame.herokuapp.com/static/img/classes/playerClasses/monk_woman.jpg';
+    const classMonk =
+        'https://dbforgame.herokuapp.com/static/img/classes/playerClasses/monk_woman.jpg';
 
-  const emptySwordLink =
-    'https://dbforgame.herokuapp.com/static/img/items/empty_cell.png';
+    const emptySwordLink =
+        'https://dbforgame.herokuapp.com/static/img/items/empty_cell.png';
 
-  const emptyItemImg =
-    'https://dbforgame.herokuapp.com/static/img/items/empty_cell.png';
+    const emptyItemImg =
+        'https://dbforgame.herokuapp.com/static/img/items/empty_cell.png';
 
-  const armorFull = [];
-  const armorSetEmpty = new Array(6).fill(emptyItemImg);
+    const armorFull = [];
+    const armorSetEmpty = new Array(6).fill(emptyItemImg);
 
-  player.armor_set.slice(0, 3).forEach((item) => armorFull.push(item));
-  //  const accessoriesSet = armorFull.push(player.accessories_set);
+    player.armor_set.slice(0, 3).forEach((item) => armorFull.push(item));
+    //  const accessoriesSet = armorFull.push(player.accessories_set);
 
-  //  console.log('-------->',accessoriesSet)
-  const armorEmptyPush = 6 - armorFull.length;
 
-  armorSetEmpty
-    .slice(0, armorEmptyPush)
-    .forEach((item) => armorFull.push({ img: item }));
+    //  console.log('-------->',accessoriesSet)
+    const armorEmptyPush = 6 - armorFull.length;
 
-  // }
-  return (
-    <div className={style.player}>
-      <ul className={style.player__items}>
-        {armorFull.map((item, i) => {
-          console.log(item);
-          return (
-            <li>
-              <EmptyItem img={item.img} />
+    armorSetEmpty
+        .slice(0, armorEmptyPush)
+        .forEach((item) => armorFull.push({img: item}));
 
-              {/* <EmptyItem height={100} width={100} img={item.img} /> */}
-            </li>
-          );
-        })}
-      </ul>
-      {/* <li> <EmptyItem height={80} width={100} /></li>
+    // }
+    return (
+        <div className={style.player}>
+            <ul className={style.player__items}>
+                {armorFull.map((item, i) => {
+                    console.log(item);
+                    return (
+                        <li>
+                            <EmptyItem img={item.img}/>
+
+                            {/* <EmptyItem height={100} width={100} img={item.img} /> */}
+                        </li>
+                    );
+                })}
+            </ul>
+            {/* <li> <EmptyItem height={80} width={100} /></li>
        <li> <EmptyItem height={80} width={100} /></li>
        <li> <EmptyItem height={80} width={100} /></li>
         <li> <EmptyItem height={80} width={100} /></li>
         <li><EmptyItem height={80} width={100} /></li> */}
 
-      <img
-        className={style.player__pic}
-        src={
-          playerClass === 'warrior'
-            ? classWarrior
-            : playerClass === 'monk'
-            ? classMonk
-            : playerClass === 'assassin'
-            ? classAssasin
-            : ''
-        }
-      ></img>
+            <img
+                className={style.player__pic}
+                src={
+                    playerClass === 'warrior'
+                        ? classWarrior
+                        : playerClass === 'monk'
+                            ? classMonk
+                            : playerClass === 'assassin'
+                                ? classAssasin
+                                : ''
+                }
+            ></img>
 
-      <div className={style.player__right}>
-        <PlayerWeapon className height={100} width={70} />
-        <div className={style.player__stats}>
-          <PlayerStats />
+            <div className={style.player__right}>
+                <PlayerWeapon className height={100} width={70}/>
+                <div className={style.player__stats}>
+                    <PlayerStats/>
+                </div>
+            </div>
+            <div className={style.player__progress}>
+                <PlayerProgressBarHpApMp
+                    className={style.header__progress_element}
+                    bgColor={'red'}
+                    height={100}
+                    stat={player?.hp}
+                    type={'player'}
+                    rotate={180}
+                    fontSize={10}
+                />
+                <PlayerProgressBarHpApMp
+                    bgColor={'green'}
+                    stat={player?.mp}
+                    height={100}
+                    type={'player'}
+                    rotate={180}
+                    fontSize={15}
+                />
+                <PlayerProgressBarHpApMp
+                    bgColor={'aqua'}
+                    stat={player?.hp}
+                    height={100}
+                    rotate={180}
+                    type={'player'}
+                    fontSize={10}
+                />
+            </div>
         </div>
-      </div>
-      <div className={style.player__progress}>
-        <PlayerProgressBarHpApMp
-          className={style.header__progress_element}
-          bgColor={'red'}
-          height={100}
-          stat={player?.hp}
-          type={'player'}
-          rotate={180}
-          fontSize={10}
-        />
-        <PlayerProgressBarHpApMp
-          bgColor={'green'}
-          stat={player?.mp}
-          height={100}
-          type={'player'}
-          rotate={180}
-          fontSize={15}
-        />
-        <PlayerProgressBarHpApMp
-          bgColor={'aqua'}
-          stat={player?.hp}
-          height={100}
-          rotate={180}
-          type={'player'}
-          fontSize={10}
-        />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Player;
