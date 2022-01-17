db79606ed04a8ab02fc57c081c0c585eb5754092
darsen <dlya_web_konkursa@mail.ru>
2022-01-17 11:14:02 +0300

    battle 09 works

--- src/redux/reducers/EnemyPlayerReducer.js
+++ src/redux/reducers/EnemyPlayerReducer.js
@@ -1,10 +1,10 @@
 import initialState from '../init/initialState'
-import {PUNCH_FROM_PLAYER_TO_ENEMY_PLAYER, SET_ENEMY_PLAYER, UNSET_ENEMY_PLAYER} from '../types/enemyPlayerTypes'
+import { PUNCH_FROM_PLAYER_TO_ENEMY_PLAYER, SET_ENEMY_PLAYER, UNSET_ENEMY_PLAYER } from '../types/enemyPlayerTypes'
 
 export const enemyPlayerReducer = (state = initialState, action) => {
     const random = Math.floor(Math.random() * 100)
     const chance = (enemyPlayerEvasion) => enemyPlayerEvasion > random
-    const enemyPlayer = {...state}
+    // const enemyPlayer = {...state}
 
     switch (action.type) {
         case SET_ENEMY_PLAYER:
@@ -14,30 +14,29 @@ export const enemyPlayerReducer = (state = initialState, action) => {
             return null
 
         case PUNCH_FROM_PLAYER_TO_ENEMY_PLAYER:
+            const {playerDamage} = action.payload
             const {battlePlayer} = action.payload
             const {battleEnemyPlayer} = action.payload
-            const {playerDamage} = action.payload
 
             // let dmg3 = player.hp - playerDamage / player.total_stats.def * 0.1
-            let dmg3 = enemyPlayer.hp - playerDamage / 2
+            let dmg3 = state.hp - (playerDamage / (state.total_stats.def * 0.1))
 
-            if (battleEnemyPlayer.defendHead === true && battlePlayer.attackHead === true) {
-                dmg3 = enemyPlayer.hp
+            if ((battleEnemyPlayer.defendHead === true) && (battlePlayer.attackHead === true)) {
+                dmg3 = state.hp
             }
-            if (battleEnemyPlayer.attackBody === true && battlePlayer.attackBody === true) {
-                dmg3 = enemyPlayer.hp
+            if ((battleEnemyPlayer.defendBody === true) && (battlePlayer.attackBody === true)) {
+                dmg3 = state.hp
             }
-            if (battleEnemyPlayer.defendLegs === true && battlePlayer.attackLegs === true) {
-                dmg3 = enemyPlayer.hp
+            if ((battleEnemyPlayer.defendLegs === true) && (battlePlayer.attackLegs === true)) {
+                dmg3 = state.hp
             }
 
-            const staminaLess = enemyPlayer.ap - 1
-            if (chance(enemyPlayer.total_stats.evs)) {
-                console.log(`${enemyPlayer.nickName} уклонился !`)
+            const staminaLess = state.ap - 1
+            if (chance(state.total_stats.evs)) {
+                console.log(`${state.nickName} уклонился !`)
                 return {...state, ap: staminaLess}
-            } else {
-                return {...state, hp: dmg3, ap: staminaLess}
             }
+            return {...state, hp: dmg3, ap: staminaLess}
 
         default:
             return state
