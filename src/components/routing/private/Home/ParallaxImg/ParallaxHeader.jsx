c6f334b9bc3d9a1951ac362347a0c969239cd466
Aleksey Mityukov <imondoro@gmail.com>
2022-01-20 22:10:48 +0300

    tt m

---
+++ src/components/routing/private/Home/ParallaxImg/ParallaxHeader.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {ParallaxBanner} from "react-scroll-parallax";
+
+const ParallaxHeader = (props) => {
+    return (
+        <ParallaxBanner
+            className="header_parallax"
+            layers={[
+                {
+                    image: props.imgsrc, amount: .35
+                },
+                {
+                    image: 'https://foo.com/bar.png',
+                    amount: 0.2,
+                },
+            ]}
+            style={{
+                height: props.height,
+            }}
+        >
+
+            <div style={
+                {
+                    boxShadow: '0 0px 100px -0px black',
+                    background: 'rgba(2,6,20,0.35)',
+                    position: "relative",
+                    alignContent: 'center'
+                }
+            }>
+                <h1 style={
+                    {
+                        fontFamily: 'Grek',
+                        color: 'rgba(250,250,250,0.8)',
+                        filter: 'drop-shadow(0px 5px 5px rgb(255, 255, 255))',
+                        fontSize: "15em",
+                        marginTop: "20%"
+                    }
+                }>{props.content}</h1>
+            </div>
+        </ParallaxBanner>
+    );
+};
+
+export default ParallaxHeader;
