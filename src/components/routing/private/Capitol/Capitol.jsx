73dcab54c46bbc7d4b8ed0f0fe4ae8c1152d9970
darsen <dlya_web_konkursa@mail.ru>
2022-01-21 15:35:28 +0300

    merged

--- src/components/routing/private/Capitol/Capitol.jsx
+++ src/components/routing/private/Capitol/Capitol.jsx
@@ -1,33 +1,38 @@
-import React from 'react';
+import React from 'react'
 
 const Capitol = () => {
     return (
-        <div className='container'>
+        <div className="container">
             <div style={{
-                margin: "auto",
+                margin: 'auto',
                 boxShadow: '0 0 10px 5px black',
                 opacity: '0.9',
                 padding: '2em',
-                borderRadius: '15%',
-                background: "wheat",
+                borderRadius: '15px',
+                background: 'wheat',
             }}>
                 <h1>Rules:</h1>
                 <article style={{
                     fontSize: '2em', paddingBottom: '2em'
                 }}>
-                    The battle technique is progressive attacks with arms and legs. To start the battle, the player just needs to tap (click) on any hostile
-                    creature and the character will rush into battle.<br/> Depending on each developed characteristic, the impact damage of a particular attack
+                    The battle technique is progressive attacks with arms and legs. To start the battle, the player just needs to
+                    tap (click) on any hostile
+                    creature and the character will rush into battle.<br/> Depending on each developed characteristic, the impact
+                    damage of a particular attack
                     will
-                    change. After defeating each of the opponents, your character will receive experience points to increase the level, as well as gold or
+                    change. After defeating each of the opponents, your character will receive experience points to increase the
+                    level, as well as gold or
                     munition.
-                    There is an additional specific in the game.<br/> The basic spell is available to you from the first entry into the world of Grimgold. The
+                    There is an additional specific in the game.<br/> The basic spell is available to you from the first entry
+                    into the world of Grimgold. The
                     rest
                     are
-                    unlocked based on game progress at levels 4 and 8. Each character class has different skills and has unique mechanics.
+                    unlocked based on game progress at levels 4 and 8. Each character class has different skills and has unique
+                    mechanics.
                 </article>
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default Capitol;
+export default Capitol
